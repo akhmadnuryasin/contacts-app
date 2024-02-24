f6cd0ff7ba5742568e6e66e6dc0674dd7e2d3cb0
Akhmad Nuryasin <akhmadnuryasin18@gmail.com>
2024-02-24 16:49:40 +0700

    combine the ContactItemImage and ContactItemBody bodies into 1 in the ContactItem.jsx file

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import ContactItemBody from "./ContactItemBody";
-import ContactItemImage from "./ContactItemImage";
 
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ children }) {
+  return <div className="contact-item">{children}</div>;
+}
+
+function ContactItemBody({ name, tag }) {
   return (
-    <div className="contact-item">
-      <ContactItemImage imageUrl={imageUrl} />
-      <ContactItemBody name={name} tag={tag} />
+    <div className="contact-item__body">
+      <h3 className="contact-item__title">{name}</h3>
+      <p className="contact-item__username">@{tag}</p>
     </div>
   );
 }
+
+function ContactItemImage({ imageUrl }) {
+  return (
+    <div className="contact-item__image">
+      <img src={imageUrl} alt="contact avatar" />
+    </div>
+  );
+}
+
+ContactItem.ContactItemBody = ContactItemBody;
+ContactItem.ContactItemImage = ContactItemImage;
 
 export default ContactItem;
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -5,7 +5,10 @@ function ContactList({ contacts }) {
   return (
     <div className="contact-list">
       {contacts.map((contact) => (
-        <ContactItem key={contact.id} {...contact} />
+        <ContactItem key={contact.id}>
+          <ContactItem.ContactItemImage imageUrl={contact.imageUrl} />
+          <ContactItem.ContactItemBody name={contact.name} tag={contact.tag} />
+        </ContactItem>
       ))}
     </div>
   );
